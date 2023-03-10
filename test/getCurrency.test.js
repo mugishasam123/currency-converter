fb8541c01d5cbcfe264a7561913acca7f02110af
mugishasam123 <mugishasamuel42@gmail.com>
2023-03-10 20:05:11 +0200

    Add test

--- test/getCurrency.test.js
+++ test/getCurrency.test.js
@@ -1,4 +1,4 @@
-const app = require('../server') // assuming that your app is exported from app.js
+const app = require('../server') // assuming that your app is exported from server.js
 const chai = require('chai')
 const chaiHttp = require('chai-http')
 const chaiFs = require('chai-fs');
